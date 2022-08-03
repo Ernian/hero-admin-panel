906f0e70120a3e2cdb7db6ea5ef62d3aa0967cb2
light <estarosa@list.ru>
2022-08-03 16:08:15 +0300

    configureStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,11 @@
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
-import { heroReducer } from '../reducers/heroReducer';
-import { filterReducer } from '../reducers/filterReducer';
+import { configureStore } from '@reduxjs/toolkit';
+import { heroReducer as heroes } from '../reducers/heroReducer';
+import { filterReducer as filters } from '../reducers/filterReducer';
 
-const rootReducer = combineReducers({
-    heroes: heroReducer,
-    filters: filterReducer,
+const store = configureStore({
+    reducer: { heroes, filters },
+    middleware: getDefaultMiddleware => getDefaultMiddleware(),
+    devTools: process.env.NODE_ENV !== 'production',
 })
-
-const store = createStore(
-    rootReducer,
-    compose(applyMiddleware(ReduxThunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-);
 
 export default store;
